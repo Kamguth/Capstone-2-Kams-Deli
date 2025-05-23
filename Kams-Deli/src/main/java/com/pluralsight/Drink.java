4ad2fdf0bdfd5e2012a90e2e9d53d06c0813172d
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-05-23 15:43:51 -0400

    Drink class finished

--- Kams-Deli/src/main/java/com/pluralsight/Drink.java
+++ Kams-Deli/src/main/java/com/pluralsight/Drink.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
 public class Drink extends MenuItem{
+
+    private String size;   // "Small", "Medium", or "Large"
+    private String flavor; // e.g., "Cola", "Lemonade", etc.
+
+    public Drink(String size, String flavor) {
+        super(size + " " + flavor + " Drink");
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        switch (size.toLowerCase()) {
+            case "small": return 2.00;
+            case "medium": return 2.50;
+            case "large": return 3.00;
+            default: return 0.0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return size + " " + flavor + " Drink - $" + String.format("%.2f", getPrice());
+    }
 }
