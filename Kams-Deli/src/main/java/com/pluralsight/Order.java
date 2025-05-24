04ffd5e09ad7be2bc2b323fbeae96ccfcc120c8f
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-05-24 11:17:39 -0400

    Order class finished

--- Kams-Deli/src/main/java/com/pluralsight/Order.java
+++ Kams-Deli/src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
-}
+
+    private List<MenuItem> items = new ArrayList<>();
+
+        public void addItem(MenuItem item) {
+            items.add(item);
+        }
+
+        public double getTotalPrice() {
+            return items.stream()
+                    .mapToDouble(MenuItem::getPrice)
+                    .sum();
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder summary = new StringBuilder();
+            summary.append("Order Summary:\n");
+
+            for (MenuItem item : items) {
+                summary.append("- ").append(item.toString()).append("\n");
+            }
+
+            summary.append("Total: $")
+                    .append(String.format("%.2f", getTotalPrice()));
+
+            return summary.toString();
+        }
+    }
+
+
