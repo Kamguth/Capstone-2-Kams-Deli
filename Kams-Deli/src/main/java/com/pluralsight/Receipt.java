b25e2ddf0ccd4c61298c0007903ffd9e3185f0f7
Kamron Guthrie <kamrontguthrie@gmail.com>
2025-05-27 13:01:18 -0400

    Receipt class almost done

--- Kams-Deli/src/main/java/com/pluralsight/Receipt.java
+++ Kams-Deli/src/main/java/com/pluralsight/Receipt.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Receipt {
+
+    public static void save(Order order) {
+        // Format: yyyyMMdd-HHmmss.txt
+        String timestamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+        String fileName = "receipt-" + timestamp + ".txt";
+
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            writer.write(order.toString());
+            writer.close();
+            System.out.println("Receipt saved as: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Failed to save receipt: " + e.getMessage());
+        }
+    }
 }
+
